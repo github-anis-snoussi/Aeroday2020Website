c9f967c6e31d81efe9cba904465f671a59b9c326
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2020-03-12 16:11:05 +0100

    almost done

--- js/main.js
+++ js/main.js
@@ -110,26 +110,4 @@
     --------------------*/
     $('.show-result-select').niceSelect();
 
-    /*------------------
-       Timetable Filter
-    --------------------*/
-    $('.timetable-controls ul li').on('click', function() {
-        var tsfilter = $(this).data('tsfilter');
-        $('.timetable-controls ul li').removeClass('active');
-        $(this).addClass('active');
-        
-        if(tsfilter == 'all') {
-            $('.classtime-table').removeClass('filtering');
-            $('.ts-item').removeClass('show');
-        } else {
-            $('.classtime-table').addClass('filtering');
-        }
-        $('.ts-item').each(function(){
-            $(this).removeClass('show');
-            if($(this).data('tsmeta') == tsfilter) {
-                $(this).addClass('show');
-            }
-        });
-    });
-
 })(jQuery);
